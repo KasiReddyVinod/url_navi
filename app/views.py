c6d0a92bf92bb3b8b83abfe00d34e2fc319af0c0
KasiReddyVinod <enfield748@gmail.com>
2023-03-16 22:17:28 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def jenny(request):
+    return render(request,'jenny.html')
+def teju(request):
+    return render(request,'teju.html')
